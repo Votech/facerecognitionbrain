e2f55b129d46ca9228dbc2eddc1eb8a32dfd41cc
unknown <mietlinski.wojciech@gmail.com>
2020-07-04 02:28:40 +0200

    Updates

---
+++ precache-manifest.731889b49137d2ed1b986b066e489db2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2406e6cc0a9632236d4f0ab5b7cbe80b",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "1ecd7559b3f8f141da23",
+    "url": "/facerecognitionbrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "207d45a2d80a4bc5fda9",
+    "url": "/facerecognitionbrain/static/css/main.dca054dc.chunk.css"
+  },
+  {
+    "revision": "1ecd7559b3f8f141da23",
+    "url": "/facerecognitionbrain/static/js/2.3bb78084.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognitionbrain/static/js/2.3bb78084.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "207d45a2d80a4bc5fda9",
+    "url": "/facerecognitionbrain/static/js/main.5fa8c62a.chunk.js"
+  },
+  {
+    "revision": "a0d072c450e7e6f545a0",
+    "url": "/facerecognitionbrain/static/js/runtime-main.3b236050.js"
+  }
+]);
