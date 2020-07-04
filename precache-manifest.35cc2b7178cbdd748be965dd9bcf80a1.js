0f17e89da9e4e8a4783243f448c2d552e37775db
unknown <mietlinski.wojciech@gmail.com>
2020-07-04 02:23:21 +0200

    Updates

---
+++ precache-manifest.35cc2b7178cbdd748be965dd9bcf80a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48a7621d14dce56d28fe414aa3657fa2",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "66c3bc2125c523cd39e9",
+    "url": "/react-gh-pages/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5bfb3af38dbc274a8ff4",
+    "url": "/react-gh-pages/static/css/main.dca054dc.chunk.css"
+  },
+  {
+    "revision": "66c3bc2125c523cd39e9",
+    "url": "/react-gh-pages/static/js/2.1b345c33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.1b345c33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bfb3af38dbc274a8ff4",
+    "url": "/react-gh-pages/static/js/main.d56257b1.chunk.js"
+  },
+  {
+    "revision": "123c52e8d727c75c8071",
+    "url": "/react-gh-pages/static/js/runtime-main.e958bcd4.js"
+  }
+]);
